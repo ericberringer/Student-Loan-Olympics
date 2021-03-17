3e9ceed5d45b75eb07d04f84aa460f033b369e55
ericberringer <eric.berringer@gmail.com>
2021-03-17 09:48:25 -0700

    now communicating with the browser

---
+++ src/PropsAndState.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export const PropsAndState = ({currentUser}) => {
+    return (
+        <>
+            <h3>Welcome to the Student Loan Olympics {currentUser}</h3>
+        </>
+    )
+}
